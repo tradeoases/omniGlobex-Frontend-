c79690faebad7cf9fb07baa7c10b98f0620289a5
Emmanuel Balikuddembe <ebalikuddembe@caa.co.ug>
2024-09-24 16:18:03 +0300

    implemented the blogs details information

--- src/pages/Blogs/components/blog-card.tsx
+++ src/pages/Blogs/components/blog-card.tsx
@@ -1,28 +1,38 @@
+import { useNavigate } from "react-router-dom";
 import UserIcon from "@/assets/user.svg";
 import commentIcon from "@/assets/comment.svg";
 import rightArrow from "@/assets/right-arrow.svg";
 
 export type BlogCommentType = {
-  commentId: string
-}
+  commentId: string;
+};
 
 export type BlogType = {
   creator: string;
-  comments: BlogCommentType[]
-  title: string
-  summary: string
-  data: string
-  id: number
-  coverImageUrl: string
-}
+  comments: BlogCommentType[];
+  title: string;
+  summary: string;
+  data: string;
+  id: number;
+  coverImageUrl: string;
+};
 
-const BlogCard = ({blog}: {blog: BlogType}) => {
-  console.log(blog)
+const BlogCard = ({ blog }: { blog: BlogType }) => {
+  const navigate = useNavigate();
+
+  const handleViewMore = () => {
+    navigate(`/blog/${blog.id}`);
+  };
+
   return (
     <div>
       <div className="blog-card-wrapper w-full border border-[#D3D3D3] ">
         <div className="img w-full h-[240px] md:h-[340px] relative bg-gray-300">
-          <img className="h-full w-full" src={blog.coverImageUrl} alt={`${blog.title} image`}/>
+          <img
+            className="h-full w-full"
+            src={blog.coverImageUrl}
+            alt={`${blog.title} image`}
+          />
         </div>
         <div className="p-[24px]">
           <div className="short-data flex space-x-9 items-center mb-3">
@@ -31,7 +41,7 @@ const BlogCard = ({blog}: {blog: BlogType}) => {
                 <img src={UserIcon} alt="user" />
               </span>
               <span className="text-xs md:text-base text-qgraytwo capitalize">
-                By {blog.creator||'Admin'}
+                By {blog.creator || "Admin"}
               </span>
             </div>
             <div className="flex space-x-1.5 items-center">
@@ -44,22 +54,23 @@ const BlogCard = ({blog}: {blog: BlogType}) => {
             </div>
           </div>
           <div className="details">
-            <h1 className="text-lg md:text-xl text-qblack hover:text-blue-500 font-semibold line-clamp-2 mb-1 capitalize">
+            <h1 className="text-lg md:text-xl text-qblack hover:text-main font-semibold line-clamp-2 mb-1 capitalize">
               {blog.title}
             </h1>
             <p className="text-gray-500 text-xs md:text-base leading-[30px] line-clamp-2 mb-3">
               {blog.summary}
             </p>
-            <a href="#">
-              <div className="flex items-center space-x-2">
+
+            <div onClick={handleViewMore} className="cursor-pointer">
+              <div className="flex items-center space-x-2 hover:underline text-main">
                 <span className="text-qblack text-xs md:text-base font-semibold">
-                  View More - needs implementation
+                  View More
                 </span>
                 <span>
                   <img src={rightArrow} alt="arrow" />
                 </span>
               </div>
-            </a>
+            </div>
           </div>
         </div>
       </div>
