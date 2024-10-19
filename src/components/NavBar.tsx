4aac6938d4029195ad3d88bdff287d6004e59306
Aiko Patrick <patrick.aikonia@gmail.com>
2024-10-19 12:17:35 +0300

    Changes that needs to be push right now

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { LuChevronDown } from "react-icons/lu";
-import { CiMenuFries, CiUser } from "react-icons/ci";
+import {  CiUser } from "react-icons/ci";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { SlHandbag } from "react-icons/sl";
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { HiMenu, HiX } from "react-icons/hi"; // Add icons for mobile menu
 import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { IUser, userStore } from "@/store/user-store";
@@ -21,7 +19,6 @@ import { HeaderCartNav } from "./header-cart-nav";
 import { navs } from "@/data/data";
 import { NavBarPagesItem } from "./navbar-page-item";
 import { useTranslation } from "react-i18next";
-import { CategoriesPopup } from "./CategoriesPopup";
 import { FaUserCircle } from "react-icons/fa";
 
 const Badge = ({ count }: { count: number }) => (
@@ -40,49 +37,16 @@ const NavBar = () => {
 
   const [userData, setUserData] = useRecoilState<IUser | null>(userStore);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+  
 
-  const [showCategory, setShowCategory] = useState<boolean>(false);
-
-  const onToggleCategory = () => {
-    setShowCategory(!showCategory);
-  };
-
-  const handleShowCat = () => {
-    showCategory ? setShowCategory(false) : setShowCategory(true);
-  };
+  
 
   return (
     <div className="hidden lg:block w-full border-b py-2 bg-main">
-      <div className="w-10/12 xl:w-8/12 mx-auto flex items-center justify-between">
+      <div className="w-10/12 xl:w-10/12 mx-auto flex items-center justify-between">
         <div className="flex items-center gap-x-6 font-semibold text-sm relative">
-          <div className="flex items-center cursor-pointer gap-x-10 p-4 rounded-t-lg bg-white relative -bottom-2 left-0 right-0">
-            <p onClick={handleShowCat} className="flex items-center gap-x-2">
-              <CiMenuFries
-                className="text-lg text-black"
-                style={{ transform: "scaleX(-1)" }}
-              />
-              <span className="whitespace-nowrap">All Categories</span>
-            </p>
-            <LuChevronDown onClick={handleShowCat} className="text-sm" />
-
-            {showCategory && (
-              <CategoriesPopup
-                onToggle={onToggleCategory}
-                isOpen={showCategory}
-              />
-            )}
-          </div>
-          {/* Hamburger Menu Icon for Mobile */}
-          <button className="lg:hidden" onClick={toggleMenu}>
-            {isMenuOpen ? (
-              <HiX className="text-white text-3xl" />
-            ) : (
-              <HiMenu className="text-white text-3xl" />
-            )}
-          </button>
+          
+         
 
           {/* Navigation Links for Desktop */}
           <div className="hidden lg:flex items-center gap-6 xl:gap-10 relative -bottom-1 flex-1">
