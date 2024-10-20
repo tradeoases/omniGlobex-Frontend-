84217ebd45fbe2062f398d1637e2518347dc4322
emmanuel <balixemma@gmail.com>
2024-10-20 17:28:12 +0300

    Fixed vercel error

--- src/pages/Authentication/login-page.tsx
+++ src/pages/Authentication/login-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
