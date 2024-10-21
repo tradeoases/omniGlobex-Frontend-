6ee2aa252d63ac62e6a74122986722bdbc0058f4
emmanuel <balixemma@gmail.com>
2024-10-21 23:13:03 +0300

    fix vercel error

--- src/pages/user/buyer-profile/BuyerDropDownProfile.tsx
+++ src/pages/user/buyer-profile/BuyerDropDownProfile.tsx
@@ -1,4 +1,3 @@
-import { CreditCard, Keyboard, LogOut, Settings, User } from "lucide-react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 // import {
 //   DropdownMenu,
@@ -27,59 +26,6 @@ export function BuyerDropDownProfile() {
   };
 
   return (
-    // <DropdownMenu.Root>
-    //   <DropdownMenu.Trigger asChild>
-    //     <div className="text-2xl cursor-pointer">
-    //       <FaUserCircle />
-    //     </div>
-    //   </DropdownMenu.Trigger>
-    //   <DropdownMenu.Content className="w-56">
-    //     {/* Main DropdownMenu */}
-
-    //     {/* Submenu Trigger for My Account */}
-    //     <DropdownMenu.Sub>
-    //       <DropdownMenu.SubTrigger>My Account</DropdownMenu.SubTrigger>
-    //       <DropdownMenu.SubContent side="bottom" align="start">
-    //         <Link to="/buyer-dashboard/myAccount/favorites">
-    //           <DropdownMenu.Item>
-    //             <User className="mr-2 h-4 w-4" />
-    //             <span>My Favorites</span>
-    //           </DropdownMenu.Item>
-    //         </Link>
-    //         <Link to="/buyer-dashboard/myAccount/preferences">
-    //           <DropdownMenu.Item>
-    //             <CreditCard className="mr-2 h-4 w-4" />
-    //             <span>Sourcing Preferences</span>
-    //           </DropdownMenu.Item>
-    //         </Link>
-    //         <Link to="/buyer-dashboard/myAccount/profile">
-    //           <DropdownMenu.Item>
-    //             <Settings className="mr-2 h-4 w-4" />
-    //             <span>User Profile</span>
-    //           </DropdownMenu.Item>
-    //         </Link>
-    //       </DropdownMenu.SubContent>
-    //     </DropdownMenu.Sub>
-
-    //     <DropdownMenu.Separator />
-
-    //     {/* Additional Menu Items */}
-    //     <Link to="/buyer-dashboard/messages">
-    //       <DropdownMenu.Item>
-    //         <Keyboard className="mr-2 h-4 w-4" />
-    //         <span className="cursor-pointer">Messages</span>
-    //       </DropdownMenu.Item>
-    //     </Link>
-
-    //     <DropdownMenu.Separator />
-
-    //     {/* Log out option */}
-    //     <DropdownMenu.Item onClick={handleLogout}>
-    //       <LogOut className="mr-2 h-4 w-4" />
-    //       <span className="cursor-pointer">Log out</span>
-    //     </DropdownMenu.Item>
-    //   </DropdownMenu.Content>
-    // </DropdownMenu.Root>
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
         <button className="IconButton" aria-label="Customise options">
